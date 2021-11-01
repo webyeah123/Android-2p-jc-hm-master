c0a0d7b49054806141137db82a198b14de948a9e
webyeah123 <93124315+webyeah123@users.noreply.github.com>
2021-11-01 19:32:56 +0800

    Update jckkz.js

--- jckkz.js
+++ jckkz.js
@@ -223,8 +223,7 @@ function lookStart(timeout = 0) {
 
                 }else{
                     console.log('\n激活看看赚任务失败')
-                    smbody = $.getdata('lookStartbody').replace(lookStartbody1 + "&", "");
-                    $.setdata(smbody, 'lookStartbody');
+                  
                     console.log("该看看赚任务已自动删除")
                 }
             } catch (e) {
