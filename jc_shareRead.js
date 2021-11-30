5b630ef4b859fdd7bea2e6b406c85b0dc2283de1
webyeah123 <93124315+webyeah123@users.noreply.github.com>
2021-11-30 16:30:06 +0800

    Update jc_shareRead.js

--- jc_shareRead.js
+++ jc_shareRead.js
@@ -20,7 +20,7 @@ let jckdCookieArr = []
 let userCk = ''
 let readCount = 0
 
-let jckdShareNum = ($.isNode() ? process.env.jckdShareNum : $.getdata('jckdShareNum')) || 3;
+let jckdShareNum = ($.isNode() ? process.env.jckdShareNum : $.getdata('jckdShareNum')) || 5;
 
 let newsItem = ''
 let UserAgent = ''
@@ -101,7 +101,7 @@ async function showmsg() {
 
 async function checkEnv() {
     
-    if(jckdShareNum == 0) {
+    if(jckdShareNum == 10) {
         console.log('当前分享次数设置为0。如果需要开启分享阅读，请设置环境变量jckdShareNum为要被阅读的次数。')
         return false
     }
